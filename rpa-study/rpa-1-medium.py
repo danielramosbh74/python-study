e37ca3f04f28c2f8c5020daee50edcac6e74818d
Daniel <danielramosbh74@gmail.com>
2024-05-14 16:40:16 -0300

    Added rpa-study

---
+++ rpa-study/rpa-1-medium.py
@@ -0,0 +1,19 @@
+import pyautogui
+import time
+
+# Aguarde alguns segundos antes de começar 
+# (tempo para abrir o aplicativo alvo)
+time.sleep(5)
+# Clique no botão "Arquivo" no menu
+pyautogui.click(x=100, y=100)
+# Espere um segundo
+time.sleep(1)
+# Clique em "Abrir" no menu suspenso
+pyautogui.click(x=100, y=150)
+# Espere um segundo
+time.sleep(1)
+# Digite o nome do arquivo a ser aberto
+pyautogui.write("exemplo.txt")
+# Pressione Enter para confirmar
+pyautogui.press("enter")
+
