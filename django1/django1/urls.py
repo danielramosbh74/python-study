205834e54f5b85b48d9e7c1b6aaaf4e96de55291
Daniel <danielramosbh74@gmail.com>
2024-11-25 15:50:13 -0300

    Atualizando

--- django1/django1/urls.py
+++ django1/django1/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for django1 project.
+"""django1 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -17,18 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-# from core.views import index, contato
+from django.conf.urls import handler404, handler500
 
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('', index),
-#     path('contato', contato)
-# ]
-
-from core.views import index, contato
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
 ]
 
+
+handler404 = views.error404
+handler500 = views.error500
