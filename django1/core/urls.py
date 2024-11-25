6ccde74491acb1163c9a5661487cf8ebfa270817
Daniel <danielramosbh74@gmail.com>
2024-11-25 14:25:05 -0300

    templates

--- django1/core/urls.py
+++ django1/core/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import index, contato
 
 urlpatterns = [
-    path('', ('index.html')),
-    path('contato', ('contato.html')),
+    path('', index),
+    path('contato', contato),
 ]
--- django1/django1/urls.py
+++ django1/django1/urls.py
@@ -15,13 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+# from core.views import index, contato
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', index),
+#     path('contato', contato)
+# ]
 
 from core.views import index, contato
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
-    path('contato', contato)
+    path('', include('core.urls')),
 ]
 
