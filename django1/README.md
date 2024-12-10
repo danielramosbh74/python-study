18a604d8b8f92a0c3863119ec46bfaeb505b3998
Daniel <danielramosbh74@gmail.com>
2024-12-10 11:12:39 -0300

    moving

--- django1/README.md
+++
@@ -1,9 +0,0 @@
-# Demonstração de projeto com aplicação básica em Django
-
-## Para executar localmente:
-
-Após baixar a pasta do projeto, django1:
-
-- No terminal, dentro da pasta django1, executar o servidor web: python3 manage.py runserver
-
-- No navegador, ir para _localhost:8000_
--- django1/django1/settings.py
+++ django1/django1/settings.py
@@ -25,9 +25,10 @@ SECRET_KEY = '%8fc-)n7mn4m%0z(b1(i+dan+ce1rs!u&mt-4xk*^f%q2)nafv'
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG com valor True = modo desenvolvimento
 # DEBUG com valor False = modo produção
-DEBUG = True
-ALLOWED_HOSTS = ['*']
-# ALLOWED_HOSTS = ['django1-gu.herokuapp.com']  # www.geekuniversity.com.br
+DEBUG = False
+
+ALLOWED_HOSTS = ['django1-gu.herokuapp.com']  # www.geekuniversity.com.br
+
 
 # Application definition
 
